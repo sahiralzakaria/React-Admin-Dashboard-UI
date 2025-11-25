66244c9c6777d1958500137309ff55a74f016e98
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-25 17:14:10 +0300

    add dummy data to chart

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,12 @@
 import Chart from '../../components/chart/Chart'
 import { FeaturedInfo } from '../../components/featuredInfo/FeaturedInfo'
 import './home.css'
-
+import { userData } from '../../dummyData'
 export const Home = () => {
     return (
         <div className='home'>
             <FeaturedInfo />
-            <Chart />
+            <Chart data={userData} title="User Analytics" grid dataKey='Active User' />
         </div>
     )
 }
