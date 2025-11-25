206a95f46ce2a8eb8e3994f9da595aa5c8ab5bf1
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-25 16:16:42 +0300

    add home page

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import { FeaturedInfo } from '../../components/featuredInfo/FeaturedInfo'
+import './home.css'
+
+export const Home = () => {
+    return (
+        <div className='home'>
+            <FeaturedInfo />
+        </div>
+    )
+}
