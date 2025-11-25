3c94b09d5fcd65ea63383fab5ec655f66bdddc27
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-25 17:09:13 +0300

    add chart to home page

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -7,7 +7,7 @@ export const FeaturedInfo = () => {
                 <span className="featuredTitle">Revanue</span>
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$2.415</span>
-                    <span className="featuredMoneyRate">-11.4 <ArrowDownward /></span>
+                    <span className="featuredMoneyRate">-11.4 <ArrowDownward className='featuredIcon negative' /></span>
                 </div>
                 <span className="featuredSub">Compared to last month</span>
             </div>
@@ -15,7 +15,7 @@ export const FeaturedInfo = () => {
                 <span className="featuredTitle">Sales</span>
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$4.415</span>
-                    <span className="featuredMoneyRate">-1.4 <ArrowDownward /></span>
+                    <span className="featuredMoneyRate">-1.4 <ArrowDownward className='featuredIcon negative' /></span>
                 </div>
                 <span className="featuredSub">Compared to last month</span>
             </div>
@@ -23,7 +23,7 @@ export const FeaturedInfo = () => {
                 <span className="featuredTitle">Cost</span>
                 <div className="featuredMoneyContainer">
                     <span className="featuredMoney">$2.225</span>
-                    <span className="featuredMoneyRate">+2.4 <ArrowUpward /></span>
+                    <span className="featuredMoneyRate">+2.4 <ArrowUpward className='featuredIcon' /></span>
                 </div>
                 <span className="featuredSub">Compared to last month</span>
             </div>
---
+++ src/dummyData.js
@@ -0,0 +1,14 @@
+export const userData = [
+    { name: 'Jan', "Active User": 4000 },
+    { name: 'Feb', "Active User": 3000 },
+    { name: 'Mar', "Active User": 5000 },
+    { name: 'Apr', "Active User": 1000 },
+    { name: 'May', "Active User": 4500 },
+    { name: 'Jun', "Active User": 3200 },
+    { name: 'Jul', "Active User": 2800 },
+    { name: 'Aug', "Active User": 5100 },
+    { name: 'Sep', "Active User": 3900 },
+    { name: 'Oct', "Active User": 4200 },
+    { name: 'Nov', "Active User": 2500 },
+    { name: 'Dec', "Active User": 4800 },
+];
