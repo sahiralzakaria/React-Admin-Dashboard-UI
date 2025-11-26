02521444b4ff1c069c03d7d81a4ed7888c2713b4
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-26 22:49:03 +0300

    add react-route

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,24 @@ import { Sidebar } from "./components/sidebar/Sidebar"
 import Topbar from "./components/topBar/TopBar"
 import './app.css';
 import { Home } from "./pages/home/Home";
+import { BrowserRouter, Routes, Route } from "react-router";
+import UserList from "./pages/userList/UserList";
 
 function App() {
 
   return (
-    <div>
+
+    <BrowserRouter>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
-      </div>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
 
+        </Routes></div>
+
+    </BrowserRouter >
 
   )
 }
