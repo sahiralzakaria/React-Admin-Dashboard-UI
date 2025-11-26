c41238363f7aa4be0a7ae10ca11f061f58c45e5e
sahir zakaria <sahir.alzakaria@gmail.com>
2025-11-27 01:42:29 +0300

    user display and update page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './app.css';
 import { Home } from "./pages/home/Home";
 import { BrowserRouter, Routes, Route } from "react-router";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
+          <Route path="/user/:userId" element={<User />} />
 
         </Routes></div>
 
---
+++ src/pages/user/User.jsx
@@ -0,0 +1,75 @@
+import './user.css'
+import { CalendarToday, LocationSearching, MailOutline, PermIdentity, PhoneAndroid } from '@mui/icons-material'
+export default function User() {
+    return (
+        <div className='user'>
+            <div className="userTitleContainer">
+                <h1 className="userTitle">Edit User</h1>
+                <button className="userAddButton">Create</button>
+            </div>
+            <div className="userContainer">
+                <div className="userShow">
+                    <div className="userShowTop">
+                        <img src="https://images.pexels.com/photos/1212984/pexels-photo-1212984.jpeg" alt="" className="userShowImg" />
+                        <div className="userShowTopTitle">
+                            <span className="userShowUsername">Ali Ahmed</span>
+                            <span className="userShowUserTitle">Software Engineer</span>
+                        </div>
+                    </div>
+                    <div className="userShowBottom">
+                        <span className="userShowTitle">Account Details</span>
+                        <div className="useShowInfo">
+                            <PermIdentity className='userShowIcon' />
+                            <span className="userShowInfoTitle">ali99</span>
+                        </div>
+                        <div className="useShowInfo">
+                            <CalendarToday className='userShowIcon' />
+                            <span className="userShowInfoTitle">06.09.2004</span>
+                        </div>
+                        <span className="userShowTitle">Contact Details</span>
+                        <div className="useShowInfo">
+                            <PhoneAndroid className='userShowIcon' />
+                            <span className="userShowInfoTitle">+1 233 354 64</span>
+                        </div>
+                        <div className="useShowInfo">
+                            <MailOutline className='userShowIcon' />
+                            <span className="userShowInfoTitle">ali99@gmail.com</span>
+                        </div>
+                        <div className="useShowInfo">
+                            <LocationSearching className='userShowIcon' />
+                            <span className="userShowInfoTitle">New York | USA</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="userUpdate">
+                    <span className="userUpdateTitle">Edit</span>
+                    <form className="userUpdateForm">
+                        <div className="userUpdateLeft">
+                            <div className="userUpdateItem">
+                                <label>Username</label>
+                                <input type="text" placeholder='ali99' className='userUpdateInput' />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Full Name</label>
+                                <input type="text" placeholder='Ali Ahmed' className='userUpdateInput' />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Email</label>
+                                <input type="text" placeholder='ali99@gmail.com' className='userUpdateInput' />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Phone</label>
+                                <input type="text" placeholder='+1 233 354 64' className='userUpdateInput' />
+                            </div>
+                            <div className="userUpdateItem">
+                                <label>Address</label>
+                                <input type="text" placeholder='New York | USA' className='userUpdateInput' />
+                            </div>
+                        </div>
+                        <div className="userUpdateRight"></div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
